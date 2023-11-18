2ffaeaff1c66461ed09a30a6ed1add8491a9edba
RickaPrincy <rckprincy@gmail.com>
2023-11-18 22:05:46 +0300

    feat: create author model

--- src/main/java/RickaPrincy/model/Author.java
+++ src/main/java/RickaPrincy/model/Author.java
@@ -1,7 +1,14 @@
 package RickaPrincy.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Author {
     private UUID id;
     private String name, ref;
--- src/main/resources/author_migration.sql
+++ src/main/resources/author_migration.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS "author" (
     "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     "name" VARCHAR(255) NOT NULL,
-    "sex" CHAR(1) CHECK (sex IN ('M', 'F')) NOT NULL
+    "ref" VARCHAR(255) NOT NULL
 );
