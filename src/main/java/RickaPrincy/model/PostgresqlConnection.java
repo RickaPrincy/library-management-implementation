8168fd1c161f248080b1b47cef53c526a39c242e
RickaPrincy <rckprincy@gmail.com>
2023-11-18 22:31:12 +0300

    feat: create postgres connection

---
+++ src/main/java/RickaPrincy/model/PostgresqlConnection.java
@@ -0,0 +1,43 @@
+package RickaPrincy.model;
+
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+@Getter
+public class PostgresqlConnection{
+    private Connection connection;
+    private Statement statement;
+    private static PostgresqlConnection oneInstance;
+    private PostgresqlConnection(){
+        try {
+            this.connection = DriverManager.getConnection(
+                    DbEnv.DB_URL,
+                    DbEnv.DB_USERNAME,
+                    DbEnv.DB_PASSWORD
+            );
+            this.statement = this.connection.createStatement();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static PostgresqlConnection getNewInstance() {
+        if(PostgresqlConnection.oneInstance == null){
+            PostgresqlConnection.oneInstance = new PostgresqlConnection();
+        }
+        return PostgresqlConnection.oneInstance;
+    }
+
+    public void closeConnection(){
+        try {
+            this.connection.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
