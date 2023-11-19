9c4371fda1a3222490072134e504af695883ce54
RickaPrincy <rckprincy@gmail.com>
2023-11-19 12:05:32 +0300

    fix: fix author migration

--- src/main/resources/create_table_author.sql
+++ src/main/resources/create_table_author.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS "author" (
     "sex" CHAR(1) CHECK (sex IN ('M', 'F')) NOT NULL
 );
 
-INSERT INTO "author" values ('authorId_1', 'authorName_1', 'authorRef_1');
-INSERT INTO "author" values ('authorId_2', 'authorName_2', 'authorRef_2');
-INSERT INTO "author" values ('authorId_3', 'authorName_3', 'authorRef_3');
+INSERT INTO "author" values ('authorId_1', 'authorName_1', 'M');
+INSERT INTO "author" values ('authorId_2', 'authorName_2', 'F');
+INSERT INTO "author" values ('authorId_3', 'authorName_3', 'M');
