2a04cad55a756efa4e78f2971e3879af7e6f9c17
RickaPrincy <rckprincy@gmail.com>
2023-11-19 11:37:19 +0300

    feat: implement saveAll and save for book

--- src/main/java/RickaPrincy/mock_test/BookMockTest.java
+++ src/main/java/RickaPrincy/mock_test/BookMockTest.java
@@ -1,11 +1,30 @@
 package RickaPrincy.mock_test;
 
+import RickaPrincy.model.Book;
+import RickaPrincy.model.Topic;
 import RickaPrincy.repository.BookCrudOperations;
+
+import java.sql.Date;
+import java.util.List;
 
 public class BookMockTest {
     private final static BookCrudOperations bookCrudOperations = new BookCrudOperations();
     public static void launch(){
         /* should return 3 books */
+        Utils.printList(bookCrudOperations.findAll());
+
+        /* should return the new book created with uuid id*/
+        System.out.println(bookCrudOperations.save(
+            new Book( "", "bookName_4", 200, Date.valueOf("2023-01-01"), List.of(Topic.COMEDY), null)
+        ));
+
+        /* should return list new author with uuid id */
+        Utils.printList(bookCrudOperations.saveAll(List.of(
+            new Book( "", "bookName_5", 200, Date.valueOf("2023-02-01"), List.of(Topic.ROMANCE, Topic.OTHER), null),
+            new Book( "", "bookName_6", 200, Date.valueOf("2023-03-01"), List.of(Topic.OTHER), null)
+        )));
+
+        /* should return 6 books */
         Utils.printList(bookCrudOperations.findAll());
     }
 }
--- src/main/java/RickaPrincy/repository/BookCrudOperations.java
+++ src/main/java/RickaPrincy/repository/BookCrudOperations.java
@@ -3,12 +3,11 @@ package RickaPrincy.repository;
 import RickaPrincy.model.Book;
 import RickaPrincy.model.Topic;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookCrudOperations implements CrudOperations<Book> {
     private final Connection connection = PostgresqlConnection.getConnection();
@@ -38,18 +37,42 @@ public class BookCrudOperations implements CrudOperations<Book> {
         } catch (SQLException error){
             System.out.println(error.getMessage());
         }
-
         return books;
     }
 
     @Override
     public List<Book> saveAll(List<Book> toSave) {
-        return null;
+        List<Book> books = new ArrayList<>();
+        toSave.forEach(el -> books.add(save(el)));
+        return books;
     }
 
     @Override
     public Book save(Book toSave) {
-        return null;
+        String topics = toSave.getTopics().stream()
+                .map(el -> "'" + el.toString() + "'::\"topic\"")
+                .collect(Collectors.joining(", "));
+        String query =
+            "INSERT INTO \"book\"(\"book_name\", \"page_numbers\", \"release_date\", \"topics\") " +
+            "VALUES (?, ? , ?, ARRAY[" + topics + "]);";
+        Book book = null;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, toSave.getBookName());
+            statement.setLong(2, toSave.getPageNumbers());
+            statement.setDate(3, toSave.getReleaseDate());
+            statement.executeUpdate();
+
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if(resultSet.next()){
+                toSave.setId(resultSet.getString(1));
+                book = toSave;
+            }
+        }catch (SQLException error){
+            System.out.println(error.getMessage());
+        }
+        return book;
     }
 
     @Override
